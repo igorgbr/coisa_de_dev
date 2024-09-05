1887af62f1442ccee51cb6c59ab6a43de2851057
igorgbr <igorgphotoarte@gmail.com>
2024-09-05 11:23:34 -0300

    refactor: ajuste de identação

--- removendo_commit/server.js
+++ removendo_commit/server.js
@@ -5,18 +5,13 @@ const app = express();
 const router = require('express').Router();
 
 // Config da API
+let corsOptions = { origin: 'http://localhost:8080' };
 
-let corsOptions = {
-    origin: 'http://localhost:8080'
-};
-
-app.use(express.json(), router, cors(corsOptions), express.urlencoded({extended: true}));
+app.use(express.json(), router, cors(corsOptions), express.urlencoded({ extended: true }));
 
 // ROTAS
-router.get('/', (_req, res) => {res.status(200).send({message: 'Hello World'})});
-
-
+router.get('/', (_req, res) => { res.status(200).send({ message: 'Hello World' }) });
 
 // config Conexão
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {console.log(`Rodando na porta ${PORT}`)});
+app.listen(PORT, () => { console.log(`Rodando na porta ${PORT}`) });
